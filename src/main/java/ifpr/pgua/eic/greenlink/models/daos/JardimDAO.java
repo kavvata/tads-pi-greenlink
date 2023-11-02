557c79fcd81aed83557f1431b1122b00af8fb59c
kavvata <gabriel.kawata@gmail.com>
2023-11-01 22:25:34 -0300

    feat: RuntimeException em JardimDAO

--- src/main/java/ifpr/pgua/eic/greenlink/models/daos/JardimDAO.java
+++ src/main/java/ifpr/pgua/eic/greenlink/models/daos/JardimDAO.java
@@ -6,7 +6,8 @@ import ifpr.pgua.eic.greenlink.models.entities.Jardim;
 
 public interface JardimDAO {
     public String cadastrarJardim(Jardim novo);
-    public ArrayList<Jardim> listarJardins();
+    public ArrayList<Jardim> listarJardins() throws RuntimeException;
+    public Jardim buscarPorId(int id) throws RuntimeException;
     public String atualizarJardim(int id, Jardim novo);
     public String removerJardim(Jardim jardim);
 }
