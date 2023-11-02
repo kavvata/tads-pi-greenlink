8f25f0bcadf8053df690a44790b4ae2d2e4dc0e1
kavvata <gabriel.kawata@gmail.com>
2023-11-01 22:23:08 -0300

    feat: tela principal

--- src/main/java/ifpr/pgua/eic/greenlink/App.java
+++ src/main/java/ifpr/pgua/eic/greenlink/App.java
@@ -1,6 +1,12 @@
 package ifpr.pgua.eic.greenlink;
 
+import ifpr.pgua.eic.greenlink.controllers.ListarJardins;
+import ifpr.pgua.eic.greenlink.models.daos.FabricaConexoes;
+import ifpr.pgua.eic.greenlink.models.daos.JDBCJardimDAO;
+import ifpr.pgua.eic.greenlink.models.daos.JardimDAO;
+import ifpr.pgua.eic.greenlink.models.repositories.RepositorioJardins;
 import io.github.hugoperlin.navigatorfx.BaseAppNavigator;
+import io.github.hugoperlin.navigatorfx.ScreenRegistryFXML;
 
 /**
  * JavaFX App
@@ -8,23 +14,27 @@ import io.github.hugoperlin.navigatorfx.BaseAppNavigator;
 
 public class App extends BaseAppNavigator {
 
+    private JardimDAO jardimDAO = new JDBCJardimDAO(FabricaConexoes.getInstance());
+    private RepositorioJardins repositorioJardins = new RepositorioJardins(jardimDAO);
+
     public static void main(String[] args) {
         launch();
     }
 
     @Override
     public String getHome() {
-        return "PRINCIPAL";
+        return "LISTARJARDINS";
     }
 
 
     @Override
     public String getAppTitle() {
-        return "Aplicativo de Tarefas!";
+        return "Greenlink";
     }
 
     @Override
     public void registrarTelas() {
+        registraTela("LISTARJARDINS", new ScreenRegistryFXML(App.class, "listar_jardins.fxml", o -> new ListarJardins(repositorioJardins)));
     }
 
 }
