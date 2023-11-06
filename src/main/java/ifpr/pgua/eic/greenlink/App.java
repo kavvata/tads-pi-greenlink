90980f755110c8a1deb917bb90088420474187c1
kavvata <gabriel.kawata@gmail.com>
2023-11-06 15:50:20 -0300

    feat: registro de telas de lista e mantimento de tarefas

--- src/main/java/ifpr/pgua/eic/greenlink/App.java
+++ src/main/java/ifpr/pgua/eic/greenlink/App.java
@@ -2,16 +2,21 @@ package ifpr.pgua.eic.greenlink;
 
 import ifpr.pgua.eic.greenlink.controllers.ListarJardins;
 import ifpr.pgua.eic.greenlink.controllers.ListarPlantas;
+import ifpr.pgua.eic.greenlink.controllers.ListarTarefas;
 import ifpr.pgua.eic.greenlink.controllers.ManterJardim;
 import ifpr.pgua.eic.greenlink.controllers.ManterPlanta;
+import ifpr.pgua.eic.greenlink.controllers.ManterTarefa;
 import ifpr.pgua.eic.greenlink.controllers.Principal;
 import ifpr.pgua.eic.greenlink.models.daos.FabricaConexoes;
 import ifpr.pgua.eic.greenlink.models.daos.JDBCJardimDAO;
 import ifpr.pgua.eic.greenlink.models.daos.JDBCPlantaDAO;
+import ifpr.pgua.eic.greenlink.models.daos.JDBCTarefaDAO;
 import ifpr.pgua.eic.greenlink.models.daos.JardimDAO;
 import ifpr.pgua.eic.greenlink.models.daos.PlantaDAO;
+import ifpr.pgua.eic.greenlink.models.daos.TarefaDAO;
 import ifpr.pgua.eic.greenlink.models.repositories.RepositorioJardins;
 import ifpr.pgua.eic.greenlink.models.repositories.RepositorioPlantas;
+import ifpr.pgua.eic.greenlink.models.repositories.RepositorioTarefas;
 import io.github.hugoperlin.navigatorfx.BaseAppNavigator;
 import io.github.hugoperlin.navigatorfx.ScreenRegistryFXML;
 
@@ -26,6 +31,9 @@ public class App extends BaseAppNavigator {
 
     private PlantaDAO plantaDAO = new JDBCPlantaDAO(FabricaConexoes.getInstance());
     private RepositorioPlantas repositorioPlantas = new RepositorioPlantas(plantaDAO, jardimDAO);
+
+    private TarefaDAO tarefaDAO = new JDBCTarefaDAO(FabricaConexoes.getInstance());
+    private RepositorioTarefas repositorioTarefas = new RepositorioTarefas(tarefaDAO, plantaDAO);
 
     public static void main(String[] args) {
         launch();
@@ -42,10 +50,6 @@ public class App extends BaseAppNavigator {
         return "Greenlink";
     }
 
-    public RepositorioJardins getRepositorioJardins() {
-        return repositorioJardins;
-    }
-
     @Override
     public void registrarTelas() {
         registraTela("PRINCIPAL", new ScreenRegistryFXML(App.class, "principal.fxml", o -> new Principal()));
@@ -56,6 +60,8 @@ public class App extends BaseAppNavigator {
         registraTela("LISTARPLANTAS", new ScreenRegistryFXML(App.class, "listar_plantas.fxml", o -> new ListarPlantas(repositorioPlantas, repositorioJardins)));
         registraTela("MANTERPLANTA", new ScreenRegistryFXML(App.class, "manter_planta.fxml", o -> new ManterPlanta(repositorioPlantas, repositorioJardins)));
 
+        registraTela("LISTARTAREFAS", new ScreenRegistryFXML(App.class, "listar_tarefas.fxml", o -> new ListarTarefas(repositorioTarefas, repositorioPlantas)));
+        registraTela("MANTERTAREFA", new ScreenRegistryFXML(App.class, "manter_tarefa.fxml", o -> new ManterTarefa(repositorioTarefas, repositorioPlantas)));
     }
 
 }
