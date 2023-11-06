293fb7b8f5ffe265124500e4a74b5fc26a03bf81
kavvata <gabriel.kawata@gmail.com>
2023-11-06 15:50:59 -0300

    feat: propriedade 'ativo' para as tabelas do banco

--- src/main/resources/ifpr/pgua/eic/greenlink/sql/init.sql
+++ src/main/resources/ifpr/pgua/eic/greenlink/sql/init.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS jardins (
     id        INTEGER PRIMARY KEY AUTOINCREMENT,
     nome      TEXT    NOT NULL,
-    descricao TEXT    NOT NULL
+    descricao TEXT    NOT NULL,
+    ativo     BOOLEAN NOT NULL
+                      DEFAULT true
 );
 
 CREATE TABLE IF NOT EXISTS plantas (
@@ -9,6 +11,24 @@ CREATE TABLE IF NOT EXISTS plantas (
     nome      TEXT    NOT NULL,
     descricao TEXT    NOT NULL,
     jardim_id INT     NOT NULL,
+    ativo     BOOLEAN NOT NULL
+                      DEFAULT true,
 
     FOREIGN KEY(jardim_id) REFERENCES jardins (id) 
 );
+
+CREATE TABLE tarefas (
+    id        INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome      TEXT    NOT NULL,
+    descricao TEXT    NOT NULL,
+    prazo     TEXT,
+    feito     BOOLEAN NOT NULL
+                      DEFAULT false,
+    planta_id INTEGER NOT NULL,
+    ativo     BOOLEAN NOT NULL
+                      DEFAULT true,
+
+    FOREIGN KEY (planta_id) REFERENCES plantas (id) 
+);
+
+-- TODO: implementar duas triggers e duas funções.
