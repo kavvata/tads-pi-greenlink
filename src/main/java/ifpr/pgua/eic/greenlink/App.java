16bd57d7400485758dbb1e64375d9e431199b130
kavvata <gabriel.kawata@gmail.com>
2023-11-09 10:48:47 -0300

    feat: Tela de login

--- src/main/java/ifpr/pgua/eic/greenlink/App.java
+++ src/main/java/ifpr/pgua/eic/greenlink/App.java
@@ -1,5 +1,6 @@
 package ifpr.pgua.eic.greenlink;
 
+import ifpr.pgua.eic.greenlink.controllers.Autenticar;
 import ifpr.pgua.eic.greenlink.controllers.ListarJardins;
 import ifpr.pgua.eic.greenlink.controllers.ListarPlantas;
 import ifpr.pgua.eic.greenlink.controllers.ListarPlantasTarefasJardim;
@@ -12,13 +13,15 @@ import ifpr.pgua.eic.greenlink.models.daos.FabricaConexoes;
 import ifpr.pgua.eic.greenlink.models.daos.JDBCJardimDAO;
 import ifpr.pgua.eic.greenlink.models.daos.JDBCPlantaDAO;
 import ifpr.pgua.eic.greenlink.models.daos.JDBCTarefaDAO;
+import ifpr.pgua.eic.greenlink.models.daos.JDBCUsuarioDAO;
 import ifpr.pgua.eic.greenlink.models.daos.JardimDAO;
 import ifpr.pgua.eic.greenlink.models.daos.PlantaDAO;
 import ifpr.pgua.eic.greenlink.models.daos.TarefaDAO;
-import ifpr.pgua.eic.greenlink.models.entities.Usuario;
+import ifpr.pgua.eic.greenlink.models.daos.UsuarioDAO;
 import ifpr.pgua.eic.greenlink.models.repositories.RepositorioJardins;
 import ifpr.pgua.eic.greenlink.models.repositories.RepositorioPlantas;
 import ifpr.pgua.eic.greenlink.models.repositories.RepositorioTarefas;
+import ifpr.pgua.eic.greenlink.models.repositories.RepositorioUsuarios;
 import ifpr.pgua.eic.greenlink.models.sessao.Sessao;
 import io.github.hugoperlin.navigatorfx.BaseAppNavigator;
 import io.github.hugoperlin.navigatorfx.ScreenRegistryFXML;
@@ -29,10 +32,12 @@ import io.github.hugoperlin.navigatorfx.ScreenRegistryFXML;
 
 public class App extends BaseAppNavigator {
 
+    private UsuarioDAO usuarioDAO = new JDBCUsuarioDAO(FabricaConexoes.getInstance(), Sessao.getInstance());
     private JardimDAO jardimDAO = new JDBCJardimDAO(FabricaConexoes.getInstance(), Sessao.getInstance());
     private PlantaDAO plantaDAO = new JDBCPlantaDAO(FabricaConexoes.getInstance(), Sessao.getInstance());
     private TarefaDAO tarefaDAO = new JDBCTarefaDAO(FabricaConexoes.getInstance(), Sessao.getInstance());
 
+    private RepositorioUsuarios repositorioUsuarios = new RepositorioUsuarios(usuarioDAO);
     private RepositorioJardins repositorioJardins = new RepositorioJardins(jardimDAO);
     private RepositorioPlantas repositorioPlantas = new RepositorioPlantas(plantaDAO, jardimDAO);
     private RepositorioTarefas repositorioTarefas = new RepositorioTarefas(tarefaDAO, plantaDAO);
@@ -43,7 +48,7 @@ public class App extends BaseAppNavigator {
 
     @Override
     public String getHome() {
-        return "PRINCIPAL";
+        return "AUTENTICAR";
     }
 
 
@@ -55,11 +60,9 @@ public class App extends BaseAppNavigator {
     @Override
     public void registrarTelas() {
 
-        /* TODO: remover */
-        Sessao sessao = Sessao.getInstance();
-        sessao.setUsuario(new Usuario(1, "admin", "admin"));
-        
         registraTela("PRINCIPAL", new ScreenRegistryFXML(App.class, "principal.fxml", o -> new Principal()));
+
+        registraTela("AUTENTICAR", new ScreenRegistryFXML(App.class, "autenticar.fxml", o -> new Autenticar(repositorioUsuarios)));
 
         registraTela("LISTARJARDINS", new ScreenRegistryFXML(App.class, "listar_jardins.fxml", o -> new ListarJardins(repositorioJardins, repositorioPlantas, repositorioTarefas)));
 
