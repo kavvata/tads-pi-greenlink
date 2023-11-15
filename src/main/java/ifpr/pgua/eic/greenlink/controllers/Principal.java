f8ec77af76249d45266511ce83436ad406e70900
kavvata <gabriel.kawata@gmail.com>
2023-11-15 00:22:29 -0300

    feat: implementação de indicador de tela atual

--- src/main/java/ifpr/pgua/eic/greenlink/controllers/Principal.java
+++ src/main/java/ifpr/pgua/eic/greenlink/controllers/Principal.java
@@ -1,6 +1,8 @@
 package ifpr.pgua.eic.greenlink.controllers;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import ifpr.pgua.eic.greenlink.App;
@@ -8,43 +10,80 @@ import ifpr.pgua.eic.greenlink.models.sessao.Sessao;
 import io.github.hugoperlin.navigatorfx.BorderPaneRegion;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 
 public class Principal implements Initializable {
 
-    @FXML
-    Label lbNomeTela;
+    private Map<String, String> nomeTelas;
 
     @FXML
-    private ButtonBar btAcoes;
+    private BorderPane rootPrincipal;
+
+    @FXML
+    private Label lbNomeTela;
+
+
+    public Principal() {
+        registraNomeTelas();
+    }
+
+    private void registraNomeTelas() {
+        nomeTelas = new HashMap<>();
+
+        nomeTelas.put("rootListarJardins", "Listar jardins");
+        nomeTelas.put("rootListarPlantas", "Listar plantas");
+        nomeTelas.put("rootListarTarefas", "Listar tarefas");
+        nomeTelas.put("rootPlantasTarefasJardim", "Plantas e Tarefas");
+        nomeTelas.put("rootManterJardim", "Manter jardim");
+        nomeTelas.put("rootManterPlanta", "Manter planta");
+        nomeTelas.put("rootManterTarefa", "Manter tarefa");
+    }
 
     @FXML
     void listarAtividades(ActionEvent event) {
         App.changeScreenRegion("LISTARTAREFAS", BorderPaneRegion.CENTER);
-        lbNomeTela.setText("Tarefas");
     }
 
     @FXML
     void listarJardins(ActionEvent event) {
         App.changeScreenRegion("LISTARJARDINS", BorderPaneRegion.CENTER);
-        lbNomeTela.setText("Jardins");
     }
 
     @FXML
     void listarPlantas(ActionEvent event) {
         App.changeScreenRegion("LISTARPLANTAS", BorderPaneRegion.CENTER);
-        lbNomeTela.setText("Plantas");
     }
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+
         Sessao s = Sessao.getInstance();
         if (s.isLogado()) {
             lbNomeTela.setText("Bem vindo, " + s.getUsuario().getNome() + "!");
         }
+
+        /*
+         * Handler que atualiza o indicador de qual tela está sendo mostrada
+         */
+
+        EventHandler<Event> handlerMudaLabel = event -> {
+            String id = rootPrincipal.getCenter().getId();
+
+            if (!nomeTelas.containsKey(id)) {
+                return;
+            }
+
+            lbNomeTela.setText(nomeTelas.get(id));
+        };
+
+        rootPrincipal.addEventHandler(ActionEvent.ACTION, handlerMudaLabel);
+        rootPrincipal.addEventHandler(MouseEvent.ANY, handlerMudaLabel);
 
         Platform.runLater(() -> listarJardins(new ActionEvent()));
     }
